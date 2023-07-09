e7ab66b838055e2143a2761f5e3b4fec8188cbaa
Mr. Ubong <127354903+Ubtech01@users.noreply.github.com>
2023-07-09 16:25:49 +0100

    Add project files.

---
+++ Controllers/Sign-upController.cs
@@ -0,0 +1,54 @@
+﻿using Hotel.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+namespace Hotel.Controllers
+{
+    public class SignupController : Controller
+    {
+        private readonly IConfiguration _configuration;
+ 
+        public SignupController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Signup(RegisteredUser registeredUser)
+         {
+
+            if (ModelState.IsValid)
+            {
+                string connectString = _configuration.GetConnectionString("Default");
+                using (SqlConnection con = new SqlConnection(connectString))
+                {
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = "INSERT INTO RegisteredUser(Id, FirstName, LastName, EmailAddress, Password, DateTime) " +
+                                      "VALUES (@Id, @FirstName, @LastName, @EmailAddress, @Password, @DateTime)";
+                    cmd.Connection = con;
+
+                    // Add parameters to the command
+                    cmd.Parameters.AddWithValue("@Id", registeredUser.Id);
+                    cmd.Parameters.AddWithValue("@FirstName", registeredUser.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", registeredUser.LastName);
+                    cmd.Parameters.AddWithValue("@EmailAddress", registeredUser.EmailAddress);
+                    cmd.Parameters.AddWithValue("@Password", registeredUser.Password);
+                    cmd.Parameters.AddWithValue("@DateTime", registeredUser.RegisteredDate);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                return RedirectToAction("Login", "Login");
+            }
+            return View();
+        }
+        
+        }
+    }
+
---
+++ Models/RegisteredUser.cs
@@ -0,0 +1,22 @@
+﻿namespace Hotel.Models
+{
+    public class RegisteredUser
+    {
+
+        public RegisteredUser()
+        {
+                Id = Guid.NewGuid();
+                RegisteredDate = DateTime.Now;
+        }
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? Password { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public RegisteredUser(Guid Id, string firstName, string lastName, string email, string password, DateTime date)
+        {
+            
+        }
+    }
+}
